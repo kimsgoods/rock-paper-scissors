a9938fdaa2d9011383835240858b7c55b7d0b52a
Kim <kimescabarte@gmail.com>
2022-08-09 09:36:14 +0800

    Updated html, css and js

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # rock-paper-scissors
 This is a rock paper scissors game made using JavaScript
 There will be 5 rounds
+
+<h2>Icon Credits</h2>
+<a href="https://www.flaticon.com/free-icons/fist" title="fist icons">Fist icons created by Freepik - Flaticon</a>
+
+<a href="https://www.flaticon.com/free-icons/hello" title="hello icons">Hello icons created by Vitaly Gorbachev - Flaticon</a>
+
+<a href="https://www.flaticon.com/free-icons/peace" title="peace icons">Peace icons created by Freepik - Flaticon</a>
--- rps.js
+++ rps.js
@@ -1,11 +1,11 @@
 const choices = document.querySelectorAll("button");
-const status = document.getElementById("status");
+const status_ = document.getElementById("status");
 const score = document.getElementById("score");
 const final = document.getElementById("final");
-let winCondition;
-let win=0;
-let lose=0;
-let round=0;
+let winCondition; //Win, Lose, or Tie
+let win=0; //counter for the player wins
+let lose=0; //counter for the computer wins
+let round=0; //counter for the number of rounds played
 
 function getComputerChoice(){
     let randomChoice=Math.floor(Math.random()*3)+1;
@@ -26,39 +26,17 @@ function getComputerChoice(){
 function playRound(playerSelection,computerSelection){
     let winCondition="";
     round++;
-
-    if(playerSelection.toUpperCase()=="ROCK"){
-        if(computerSelection.toUpperCase()=="ROCK"){
-            winCondition="TIE";
-        }else if(computerSelection.toUpperCase()=="PAPER"){
-            winCondition="LOSE";
-        }else{
-            winCondition="WIN";
-        }
-    }else if(playerSelection.toUpperCase()=="PAPER"){
-        if(computerSelection.toUpperCase()=="ROCK"){
-            winCondition="WIN";
-        }else if(computerSelection.toUpperCase()=="PAPER"){
-            winCondition="TIE";
-        }else{
-            winCondition="LOSE";
-        }
-
-    }else if(playerSelection.toUpperCase()=="SCISSORS"){
-        if(computerSelection.toUpperCase()=="ROCK"){
-            winCondition="LOSE";
-        }else if(computerSelection.toUpperCase()=="PAPER"){
-            winCondition="WIN";
-        }else{
-            winCondition="TIE";
-        }
-
+    if((playerSelection.toUpperCase()=="ROCK" && computerSelection.toUpperCase()=="SCISSORS")
+     ||(playerSelection.toUpperCase()=="PAPER" && computerSelection.toUpperCase()=="ROCK")
+     ||(playerSelection.toUpperCase()=="SCISSORS" && computerSelection.toUpperCase()=="PAPER")){
+        winCondition="WIN";
+    }else if(playerSelection.toUpperCase() === computerSelection.toUpperCase()){
+        winCondition="TIE";
     }else{
-        console.log("Invalid input you idiot!")
-        return;
+        winCondition="LOSE";
     }
     console.log("You used "+playerSelection.toUpperCase()+" against the computer's "+computerSelection.toUpperCase());
-    status.innerText=`You used ${playerSelection.toUpperCase()} against the computer's ${computerSelection.toUpperCase()}`;
+    status_.innerText=`You used ${playerSelection.toUpperCase()} against the computer's ${computerSelection.toUpperCase()}`;
     console.log(winCondition);
     final.innerText=`Round ${round}: ${winCondition}`;
     return winCondition;
@@ -78,7 +56,7 @@ function game(){
     console.log(`You:${win} | Computer: ${lose}`);
     score.innerText=`You: ${win} | Computer: ${lose}`;
     if(win == 5 || lose == 5){
-        choices.forEach(choice => choice.disabled=true);
+        choices.forEach(choice => choice.disabled = true); //disable buttons when either wins
         if(win>lose){
             console.log("YOU WIN!");
             final.innerText = "YOU WIN!";
